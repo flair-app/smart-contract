9ab7f517be695c4a0a253c999c3f9323a78a9c90
calebmcelroy <calebmcelroy@gmail.com>
2019-09-26 15:47:06 -0400

    tests & implementation: createcat & editcat

--- src/flair.cpp
+++ src/flair.cpp
@@ -1,16 +1,61 @@
 #include <eosio/eosio.hpp>
 #include <eosio/print.hpp>
-
+#include <string>
 
 using namespace eosio;
 
-class [[eosio::contract("flair")]] hello : public contract {
+
+
+class [[eosio::contract("flair")]] flair : public contract {
   public:
       using contract::contract;
 
+      /*
+         CREATE CATEGORY
+      */
       [[eosio::action]]
-      void hi( name user ) {
-         require_auth( user );
-         print( "Hello, ", user);
+      void createcat(name id,  std::string name) {
+         require_auth( _self );
+
+         category_index categories( _self, _self.value );
+
+         categories.emplace(_self, [&](category& row) {
+            row.id = id;
+            row.name = name;
+            row.archived = false;
+         });
       }
+
+      /*
+         EDIT CATEGORY
+      */
+      struct editcatargs {
+         std::string name;
+         bool archived;
+      };
+
+      [[eosio::action]]
+      void editcat(name id,  editcatargs data) {
+         require_auth( _self );
+
+         category_index categories( _self, _self.value );
+
+         auto iterator = categories.find(id.value);
+         categories.modify(iterator, _self, [&](category& row) {
+            row.id = id;
+            row.name = data.name;
+            row.archived = data.archived;
+         });
+      }
+
+   private:
+      struct [[eosio::table]] category {
+         name id;
+         std::string name;
+         bool archived;
+
+         uint64_t primary_key() const { return id.value; }
+      };
+
+      typedef eosio::multi_index<"categories"_n, category> category_index;
 };
---
+++ tests/test_category_actions.py
@@ -0,0 +1,137 @@
+import unittest, sys, json
+from eosfactory.eosf import *
+
+verbosity([Verbosity.INFO, Verbosity.OUT])
+
+CONTRACT_WORKSPACE = sys.path[0] + "/../"
+
+# Actors of the test:
+MASTER = MasterAccount()
+HOST = Account()
+ALICE = Account()
+
+class CategoryActionsUnitTest(unittest.TestCase):
+
+    def run(self, result=None):
+        super().run(result)
+
+    @classmethod
+    def setUpClass(cls):
+        reset()
+        create_master_account("MASTER")
+
+        create_account("HOST", MASTER)
+        smart = Contract(HOST, CONTRACT_WORKSPACE)
+        smart.build()
+        smart.deploy()
+
+        create_account("ALICE", MASTER)
+
+    def setUp(self):
+        pass
+
+    def test_create_category_saves_fields_into_table(self):
+        HOST.push_action(
+            "createcat", 
+            {
+                "id":"music",
+                "name":"Music",
+            }, 
+            permission=(HOST, Permission.ACTIVE)
+        )
+
+        catRes = HOST.table("categories", HOST, lower="music", upper="music")
+        catData = json.loads(catRes.out_msg)
+
+        self.assertEqual(catData["rows"], [{
+            "id":"music",
+            "name":"Music",
+            "archived": False,
+        }])
+            
+
+    def test_create_category_requires_auth_of_self(self):
+        with self.assertRaises(MissingRequiredAuthorityError):
+            HOST.push_action(
+                "createcat", 
+                {
+                    "id":"music",
+                    "name":"Music",
+                }, 
+                permission=(ALICE, Permission.ACTIVE)
+            )
+
+    def test_create_category_must_be_unique_id(self):
+        HOST.push_action(
+            "createcat", 
+            {
+                "id":"music2",
+                "name":"Music 2a",
+            }, 
+            permission=(HOST, Permission.ACTIVE),
+            force_unique=1
+        )
+
+        with self.assertRaises(Error):
+            HOST.push_action(
+                "createcat", 
+                {
+                    "id":"music2",
+                    "name":"Music 2b",
+                }, 
+                permission=(HOST, Permission.ACTIVE),
+                force_unique=1
+            )
+
+    def test_edit_category_modifies_table(self):
+        HOST.push_action(
+            "createcat", 
+            {
+                "id":"editexample",
+                "name":"Edit Example 1",
+            },
+            permission=(HOST, Permission.ACTIVE)
+        )
+        
+        HOST.push_action(
+            "editcat", 
+            {
+                "id":"editexample",
+                "data": {
+                    "name":"Edit Example 2",
+                    "archived": True,
+                },
+            },
+            permission=(HOST, Permission.ACTIVE)
+        )
+
+        catRes = HOST.table("categories", HOST, lower="editexample", upper="editexample")
+        catData = json.loads(catRes.out_msg)
+
+        self.assertEqual(catData["rows"], [{
+            "id":"editexample",
+            "name":"Edit Example 2",
+            "archived": True,
+        }])
+
+    def test_edit_category_requires_auth_self(self):
+        with self.assertRaises(MissingRequiredAuthorityError):
+            HOST.push_action(
+                "editcat", 
+                {
+                    "id":"music",
+                    "data": {
+                        "name":"Music",
+                        "archived": False,
+                    },
+                },
+                permission=(ALICE, Permission.ACTIVE)
+            )
+
+    @classmethod
+    def tearDownClass(cls):
+        stop()
+
+
+if __name__ == "__main__":
+    unittest.main()
